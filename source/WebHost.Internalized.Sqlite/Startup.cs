2b606ca0588b6e6a83aa9d766024e59dca632459
John10 Korsnes <johnkors@gmail.com>
2016-05-22 18:16:33 +0200

    Added barebones webhost w minimium references to be a web host (not installed pingowin yet)

---
+++ source/WebHost.Internalized.Sqlite/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebHost.Internalized.Sqlite.Startup))]
+
+namespace WebHost.Internalized.Sqlite
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+        }
+    }
+}
